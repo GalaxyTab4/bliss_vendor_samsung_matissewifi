5975eff61274680d95fbaed44a0974e2e2ad98ce
sub77 <sub77@ymail.com>
2015-08-24 23:45:20 +0200

    matisse3g -> matissewifi

--- matissewifi-vendor.mk
+++ matissewifi-vendor.mk
@@ -19,6 +19,6 @@ PRODUCT_PACKAGES += \
     libHevcSwDecoder \
     TimeService
 
-$(call inherit-product, vendor/samsung/matisse3g/matisse3g-vendor-blobs.mk)
+$(call inherit-product, vendor/samsung/matissewifi/matissewifi-vendor-blobs.mk)
 
 
